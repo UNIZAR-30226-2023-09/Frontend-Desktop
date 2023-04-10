dad96da8c09181209a47b9ec8240a51e02203fb0
marcospg26 <820532@unizar.es>
2023-04-10 19:32:19 +0200

    añadir propiedades a lista propiedades

--- src/main/java/ListaPropiedadesController.java
+++ src/main/java/ListaPropiedadesController.java
@@ -7,7 +7,6 @@
  -----------------------------------------------------------------------
 */
 import java.net.URL;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
