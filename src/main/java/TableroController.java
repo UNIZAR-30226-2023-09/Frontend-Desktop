c6e671e3a98d8c26e05811a79a796e5159d02a9c
alexsanz66 <jandrolodon@gmail.com>
2023-04-10 16:38:32 +0200

    Reordenacion tableroController

--- src/main/java/TableroController.java
+++ src/main/java/TableroController.java
@@ -40,12 +40,12 @@ public class TableroController implements Initializable {
 
     private Timeline timeline;
 
-    @FXML
-    public void tirarDados(MouseEvent e) // HAY QUE COMPROBAR QUE SEA NUESTRO TURNO
-    {   
-        ImageView imagenDado = (ImageView) e.getSource();
+    private void partida(){
+
         //while(GestionPartida.enPartida){
-            ConexionServidor.esperar();
+            //ConexionServidor.esperar();       //HABRA QUE PONERLO DONDE PEREZ
+            dado1.setDisable(true);
+            dado2.setDisable(true);
             if (GestionPartida.miTurno == true) {
 
                 System.out.println("miTurno?");
@@ -56,142 +56,150 @@ public class TableroController implements Initializable {
                 while (GestionPartida.CuentaInfoRecibida < 3) {
                     System.out.println("cuentaInfoRecibida?2");
                     System.out.println(GestionPartida.CuentaInfoRecibida);
-
                     ConexionServidor.esperar();
-
-                    //AQUI ACTUALIZAREMOS LA INFORMACION POR AHORA
-                
                 }
-
-                //dado1.setDisable(false);
-                //dado2.setDisable(false);
-                GestionPartida.CuentaInfoRecibida = 0;
-
+                 
                 do {
-                    if (imagenDado.getId().equals("dado1") || imagenDado.getId().equals("dado2")) {
-                        GestionPartida.lanzarDados(GestionPartida.nombreUser,GestionPartida.IDPartida);
-
-                        ConexionServidor.esperar();
-
-                        Thread threadL = new Thread() {
-                            public void run() {
-                                
-                                try {
-                                    for (int i = 0; i < 15; i++) {
-                                        File file = new File("src/main/resources/Dice" + (random.nextInt(6) + 1) + ".png");
-                                        dado1.setImage(new Image(file.toURI().toString()));
-                                        Thread.sleep(50);
-                                    }
-                                    File file = new File("src/main/resources/Dice" + GestionPartida.dados[0] + ".png");
-                                    dado1.setImage(new Image(file.toURI().toString()));
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        };
-
-                        Thread threadR = new Thread() {
-                            public void run() {
-                                
-                                try {
-                                    for (int i = 0; i < 15; i++) {
-                                        File file = new File("src/main/resources/Dice" + (random.nextInt(6) + 1) + ".png");
-                                        dado2.setImage(new Image(file.toURI().toString()));
-                                        Thread.sleep(50);
-                                    }
-                                    File file = new File("src/main/resources/Dice"+ GestionPartida.dados[1] + ".png");
-                                    dado2.setImage(new Image(file.toURI().toString()));
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        };
-
-                        threadL.start();
-                        threadR.start();
-
-                        System.out.println("dado1");
-                        System.out.println(GestionPartida.dados[0]);
-
-                        System.out.println("dado2");
-                        System.out.println(GestionPartida.dados[1]);
-
-                        if(GestionPartida.enCarcel == false){    
-
-                            String posi = "Pos" + String.valueOf(GestionPartida.posicionesJugadores[GestionPartida.indiceJugador]);
-                            Integer jug = GestionPartida.indiceJugador;
-
-                            System.out.println("posi");
-                            System.out.println(GestionPartida.posicionesJugadores[GestionPartida.indiceJugador]);
-
-                            String coordenadas;
-                            switch (jug) {
-                                    case 0:
-                                        coordenadas = DatosPartida.mapaPropiedades1.get(posi);
-                                        break;
-                                    case 1:
-                                        coordenadas = DatosPartida.mapaPropiedades2.get(posi);
-                                        break;
-                                    case 2:
-                                        coordenadas = DatosPartida.mapaPropiedades3.get(posi);
-                                        break;
-                                    case 3:
-                                        coordenadas = DatosPartida.mapaPropiedades4.get(posi);
-                                        break;
-                                    default:
-                                        coordenadas = "ERROR";
-                                        System.out.println("ERROR CASILLA1");
-                                        break;
-                            }
-                            
-                            String[] partes = coordenadas.split(",");
-                            int x = Integer.parseInt(partes[0]);
-                            int y = Integer.parseInt(partes[1]);
-                            
-
-                            switch (jug) {
-                                case 0:
-                                    user1.setLayoutX(x);
-                                    user1.setLayoutY(y);
-                                    break;
-                                case 1:
-                                    user2.setLayoutX(x);
-                                    user2.setLayoutY(y);
-                                    break;
-                                case 2:
-                                    user3.setLayoutX(x);
-                                    user3.setLayoutY(y);
-                                    break;
-                                case 3:
-                                    user4.setLayoutX(x);
-                                    user4.setLayoutY(y); 
-                                    break; 
-                                default:
-                                    System.out.println("ERROR CASILLA2");
-                                    break;
-                            }
-
-                            //>AQUI HABRA QUE HACER LO DE COMPRAR PROPIEDADES Y TAL
-                        }
-                    } 
-                    System.out.println("DadosDobles?");
-                    System.out.println(GestionPartida.dadosDobles);
+                    dado1.setDisable(false);
+                    dado2.setDisable(false);
+                    
+                    //tirarDados(e); //ImageView imagenDado  MouseEvent e
+                    ConexionServidor.esperar();
+                    //while(esperandoTirar){}
+                    //dado1.setDisable(true);
+                    //dado2.setDisable(true);
                 } while(GestionPartida.dadosDobles);
-
+                
                 GestionPartida.finTurno(GestionPartida.client);
                 GestionPartida.miTurno = false;
                 System.out.println("finTurno?");
                 System.out.println(GestionPartida.miTurno);
                 System.out.println(" ");
                 System.out.println(" ");
-                            
-                
+
             }
-            //dado1.setDisable(true);
-            //dado2.setDisable(true);
+            ConexionServidor.esperar();
+        //}    
+    }
+
+    @FXML
+    public void tirarDados(MouseEvent e) // HAY QUE COMPROBAR QUE SEA NUESTRO TURNO
+    {   
+        System.out.println("entro2");
+        GestionPartida.CuentaInfoRecibida = 0;
+        ImageView imagenDado = (ImageView) e.getSource();
+        if (imagenDado.getId().equals("dado1") || imagenDado.getId().equals("dado2")) {
+            GestionPartida.lanzarDados(GestionPartida.nombreUser,GestionPartida.IDPartida);
 
             ConexionServidor.esperar();
-        //}
+
+            Thread threadL = new Thread() {
+                public void run() {
+                    
+                    try {
+                        for (int i = 0; i < 15; i++) {
+                            File file = new File("src/main/resources/Dice" + (random.nextInt(6) + 1) + ".png");
+                            dado1.setImage(new Image(file.toURI().toString()));
+                            Thread.sleep(50);
+                        }
+                        File file = new File("src/main/resources/Dice" + GestionPartida.dados[0] + ".png");
+                        dado1.setImage(new Image(file.toURI().toString()));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            Thread threadR = new Thread() {
+                public void run() {
+                    
+                    try {
+                        for (int i = 0; i < 15; i++) {
+                            File file = new File("src/main/resources/Dice" + (random.nextInt(6) + 1) + ".png");
+                            dado2.setImage(new Image(file.toURI().toString()));
+                            Thread.sleep(50);
+                        }
+                        File file = new File("src/main/resources/Dice"+ GestionPartida.dados[1] + ".png");
+                        dado2.setImage(new Image(file.toURI().toString()));
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            threadL.start();
+            threadR.start();
+
+            System.out.println("dado1");
+            System.out.println(GestionPartida.dados[0]);
+
+            System.out.println("dado2");
+            System.out.println(GestionPartida.dados[1]);
+
+            if(GestionPartida.enCarcel == false){    
+
+                String posi = "Pos" + String.valueOf(GestionPartida.posicionesJugadores[GestionPartida.indiceJugador]);
+                Integer jug = GestionPartida.indiceJugador;
+
+                System.out.println("posi");
+                System.out.println(GestionPartida.posicionesJugadores[GestionPartida.indiceJugador]);
+
+                String coordenadas;
+                switch (jug) {
+                        case 0:
+                            coordenadas = DatosPartida.mapaPropiedades1.get(posi);
+                            break;
+                        case 1:
+                            coordenadas = DatosPartida.mapaPropiedades2.get(posi);
+                            break;
+                        case 2:
+                            coordenadas = DatosPartida.mapaPropiedades3.get(posi);
+                            break;
+                        case 3:
+                            coordenadas = DatosPartida.mapaPropiedades4.get(posi);
+                            break;
+                        default:
+                            coordenadas = "ERROR";
+                            System.out.println("ERROR CASILLA1");
+                            break;
+                }
+                        
+                String[] partes = coordenadas.split(",");
+                int x = Integer.parseInt(partes[0]);
+                int y = Integer.parseInt(partes[1]);
+                    
+                switch (jug) {
+                    case 0:
+                        user1.setLayoutX(x);
+                        user1.setLayoutY(y);
+                        break;
+                    case 1:
+                        user2.setLayoutX(x);
+                        user2.setLayoutY(y);
+                        break;
+                    case 2:
+                        user3.setLayoutX(x);
+                        user3.setLayoutY(y);
+                        break;
+                    case 3:
+                        user4.setLayoutX(x);
+                        user4.setLayoutY(y); 
+                        break; 
+                    default:
+                        System.out.println("ERROR CASILLA2");
+                        break;
+                }
+            }
+        }
+        while (!GestionPartida.meToca) {
+            ConexionServidor.esperar();
+        }
+        GestionPartida.meToca = false; 
+
+        System.out.println("DadosDobles?");
+        System.out.println(GestionPartida.dadosDobles);
+        System.out.println(" ");
+        System.out.println(" ");
     }
 
     @Override
@@ -208,9 +216,10 @@ public class TableroController implements Initializable {
             datosPartida.setVisible(true);
             chat.setVisible(false);
 
+            partida();
             
             timeline = new Timeline();
-            Duration interval = Duration.seconds(1);
+            Duration interval = Duration.seconds(3);
             KeyFrame keyFrame = new KeyFrame(interval, event -> {
                 if (!estamosActualizando) {
                     actualizar();
@@ -219,7 +228,7 @@ public class TableroController implements Initializable {
             timeline.getKeyFrames().add(keyFrame);
             timeline.setCycleCount(Timeline.INDEFINITE);
             timeline.play();
-            
+              
 
         } catch (IOException e) {
             e.printStackTrace();
