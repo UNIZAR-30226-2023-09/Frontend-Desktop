2d3d3fc884b7f7051add662ae16865c0d4b634cd
alexsanz66 <jandrolodon@gmail.com>
2023-04-11 18:14:52 +0200

    CompraPropiedad

--- src/main/java/ComprarPropiedadController.java
+++ src/main/java/ComprarPropiedadController.java
@@ -1,13 +1,19 @@
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.Semaphore;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class ComprarPropiedadController {
+public class ComprarPropiedadController implements Initializable{
 
     @FXML
     private Button btnComprar, btnRechazar;
+
+    public static Semaphore semaphoreComprar = new Semaphore(0);
 
     @FXML
     public void actionEvent(ActionEvent e) throws IOException {
@@ -20,6 +26,18 @@ public class ComprarPropiedadController {
             // abrir la tienda
             App.setRoot("Tienda");
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
+
+    public static void gestionarCompraPropiedad(){
+        TableroController.datosPartida.setVisible(false);
+        TableroController.chat.setVisible(false);
+        TableroController.propiedad.setVisible(true);
     }
     
 }
--- src/main/java/TableroController.java
+++ src/main/java/TableroController.java
@@ -28,9 +28,9 @@ public class TableroController implements Initializable {
     private ImageView dado1, dado2, user1, user2, user3, user4;
 
     @FXML
-    private VBox datosPartida;
+    public static VBox datosPartida;
 
-    private VBox listaJugadores, listaPropiedades, chat, propiedad;
+    public static VBox listaJugadores, listaPropiedades, chat, propiedad;
 
     @FXML
     private Button btnChat;
@@ -71,13 +71,13 @@ public class TableroController implements Initializable {
                     }
                     
                     //AQUI VAMOS A GESTIONAR EN QUE CASILLA HEMOS CAIDO PARA COMPRAR, BANCO Y CASINO
+                    
+                    ComprarPropiedadController.gestionarCompraPropiedad();
 
                     if (!GestionPartida.enCarcel) {
                         if (GestionPartida.comprarPropiedad) {
                             //AQUI PONER QUE LA PANTALLA DE COMPRA SE INICIE
-                            datosPartida.setVisible(false);
-                            chat.setVisible(false);
-                            propiedad.setVisible(true);
+                            
                             //SEMAFORO DE COMPRA
                         } else if (GestionPartida.apostarDinero) {
                             //AQUI PONER QUE LA PANTALLA DE CASINO
