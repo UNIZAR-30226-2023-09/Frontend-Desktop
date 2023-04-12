26cc16297f8ff2f231b87bff77b2c77f8e0c2681
marcospg26 <820532@unizar.es>
2023-04-12 18:55:30 +0200

    controlador del la rule

--- src/main/java/CasinoController.java
+++ src/main/java/CasinoController.java
@@ -1,59 +1,92 @@
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.lang.model.util.ElementScanner14;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 
-public class CasinoController {
+public class CasinoController implements Initializable{
 
     @FXML
     private TextField txtDinero;
 
     @FXML
-    private Button btnApostar;
+    private Button btnApostar, btnRetirarse;
+
+    @FXML
+    private Label lblGanancias, lblError;
 
     @FXML
     public void actionEvent(ActionEvent e) throws IOException {
         Object evt = e.getSource();
-        /*
-        Thread threadL = new Thread() {
-            public void run() {
-                
-                try {
-                    for (int i = 0; i < 15; i++) {
-                        File file = new File("src/main/resources/Dice" + (random.nextInt(6) + 1) + ".png");
-                        dado1.setImage(new Image(file.toURI().toString()));
-                        Thread.sleep(50);
-                    }
-                    File file = new File("src/main/resources/Dice" + GestionPartida.dados[0] + ".png");
-                    dado1.setImage(new Image(file.toURI().toString()));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+        // comprobar si ha decidido apostar o es un cagon
+
+        if(evt.equals(btnApostar))
+        {
+            // mirar que haya una cantidad valida
+
+            if(Integer.parseInt(txtDinero.toString()) > GestionPartida.dineroEnBanco)
+            {
+                // indicar que no tienes suficiente saldo
+                lblError.setVisible(true);
+                lblError.setText("No tienes el suficiente saldo");
+            }
+            else if(Integer.parseInt(txtDinero.toString()) <= 0)
+            {
+                // indicar que tiene que introducir una cantidad no nula
+                lblError.setVisible(true);
+                lblError.setText("Debes introducir una cantidad estrictamente superior a 0$");
+            }
+            else 
+            {
+                int dineroAntes = GestionPartida.dineroEnBanco;
+                // enviar msj de mirar si gana
+
+                GestionPartida.apostarDinero(GestionPartida.client, txtDinero.toString());
+
+                // esperar respuesta
+                ConexionServidor.esperar();
+
+                if(dineroAntes < GestionPartida.dineroEnBanco)
+                {
+                    // si ganamos mostramos el dinero obtenido
+                    lblGanancias.setStyle("-fx-text-fill: green;");
+                    lblGanancias.setText("+" + Integer.toString(Integer.parseInt(txtDinero.toString())*2) + "$");
+                }
+                else
+                {
+                    // si perdemos msotramos el dinero que se ha restado
+                    lblGanancias.setStyle("-fx-text-fill: red;");
+                    lblGanancias.setText("-" + txtDinero.toString() + "$");
                 }
             }
-        };
+        }
+    }
 
-        Thread threadR = new Thread() {
-            public void run() {
-                
-                try {
-                    for (int i = 0; i < 15; i++) {
-                        File file = new File("src/main/resources/Dice" + (random.nextInt(6) + 1) + ".png");
-                        dado2.setImage(new Image(file.toURI().toString()));
-                        Thread.sleep(50);
-                    }
-                    File file = new File("src/main/resources/Dice"+ GestionPartida.dados[1] + ".png");
-                    dado2.setImage(new Image(file.toURI().toString()));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Crea un TextFormatter para valores enteros y establece un Filter para aceptar solo valores numéricos
+        TextFormatter<Integer> textFormatter = new TextFormatter<Integer>((Change c) -> {
+            if (c.getControlNewText().matches("\\d*")) {
+                return c;
+            } else {
+                return null;
             }
-        };
+        });
+        // Establece el TextFormatter en el TextField
+        txtDinero.setTextFormatter(textFormatter);
 
-        threadL.start();
-        threadR.start();
-        */
+        // ocultamos el mensaje de error
+        lblError.setVisible(false);
     }
     
 }
