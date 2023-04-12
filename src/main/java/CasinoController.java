78eb606a2423937ddbc0770da0c3afb7af07569b
marcospg26 <820532@unizar.es>
2023-04-12 22:17:29 +0200

    ottersportium

--- src/main/java/CasinoController.java
+++ src/main/java/CasinoController.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
 import java.net.URL;
+import javafx.util.Duration;
 import java.util.ResourceBundle;
 
-import javax.lang.model.util.ElementScanner14;
-
+import javafx.animation.KeyFrame;
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
 import javafx.scene.control.TextFormatter.Change;
+import javafx.scene.image.ImageView;
 
 public class CasinoController implements Initializable{
 
@@ -25,22 +28,19 @@ public class CasinoController implements Initializable{
     private Label lblGanancias, lblError;
 
     @FXML
-    public void actionEvent(ActionEvent e) throws IOException {
+    private ImageView imgRule;
+
+    @FXML
+    public void actionEvent(ActionEvent e) throws IOException
+    {
         Object evt = e.getSource();
-
+    
         // comprobar si ha decidido apostar o es un cagon
-
-        if(evt.equals(btnApostar))
+    
+        if (evt.equals(btnApostar))
         {
             // mirar que haya una cantidad valida
-
-            if(Integer.parseInt(txtDinero.toString()) > GestionPartida.dineroEnBanco)
-            {
-                // indicar que no tienes suficiente saldo
-                lblError.setVisible(true);
-                lblError.setText("No tienes el suficiente saldo");
-            }
-            else if(Integer.parseInt(txtDinero.toString()) <= 0)
+            if (txtDinero.getText().equals(""))
             {
                 // indicar que tiene que introducir una cantidad no nula
                 lblError.setVisible(true);
@@ -48,29 +48,55 @@ public class CasinoController implements Initializable{
             }
             else 
             {
-                int dineroAntes = GestionPartida.dineroEnBanco;
-                // enviar msj de mirar si gana
-
-                GestionPartida.apostarDinero(GestionPartida.client, txtDinero.toString());
-
-                // esperar respuesta
-                ConexionServidor.esperar();
-
-                if(dineroAntes < GestionPartida.dineroEnBanco)
+                int apuesta = Integer.parseInt(txtDinero.getText());
+                if (apuesta > GestionPartida.dineroEnBanco)
                 {
-                    // si ganamos mostramos el dinero obtenido
-                    lblGanancias.setStyle("-fx-text-fill: green;");
-                    lblGanancias.setText("+" + Integer.toString(Integer.parseInt(txtDinero.toString())*2) + "$");
+                    // indicar que no tienes suficiente saldo
+                    lblError.setVisible(true);
+                    lblError.setText("No tienes el suficiente saldo");
+                } 
+                else if (apuesta <= 0)
+                {
+                    // indicar que tiene que introducir una cantidad no nula
+                    lblError.setVisible(true);
+                    lblError.setText("Debes introducir una cantidad estrictamente superior a 0$");
                 }
                 else
                 {
-                    // si perdemos msotramos el dinero que se ha restado
-                    lblGanancias.setStyle("-fx-text-fill: red;");
-                    lblGanancias.setText("-" + txtDinero.toString() + "$");
+                    int dineroAntes = GestionPartida.dineroEnBanco;
+                    // enviar msj de mirar si gana
+                    GestionPartida.apostarDinero(GestionPartida.client, txtDinero.toString());
+        
+                    // Inicia la animación de rotación
+                    RotateTransition rotacion = new RotateTransition(Duration.seconds(2), imgRule);
+                    rotacion.setByAngle(360);
+                    rotacion.play();
+        
+                    // Espera un tiempo para que la ruleta gire
+                    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), e2 -> {
+                        rotacion.pause();
+        
+                        // Esperar respuesta
+                        ConexionServidor.esperar();
+
+                        lblGanancias.setVisible(true);
+        
+                        if (dineroAntes < GestionPartida.dineroEnBanco) {
+                            // si ganamos mostramos el dinero obtenido
+                            lblGanancias.setStyle("-fx-text-fill: green;");
+                            lblGanancias.setText("+" + Integer.toString(apuesta*2) + "$");
+                        } else {
+                            // si perdemos msotramos el dinero que se ha restado
+                            lblGanancias.setStyle("-fx-text-fill: red;");
+                            lblGanancias.setText("-" + txtDinero.getText() + "$");
+                        }
+                    }));
+                    timeline.play();
                 }
             }
         }
     }
+    
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -85,8 +111,9 @@ public class CasinoController implements Initializable{
         // Establece el TextFormatter en el TextField
         txtDinero.setTextFormatter(textFormatter);
 
-        // ocultamos el mensaje de error
+        // ocultamos el mensaje de error y del dinero obtenido/perdido
         lblError.setVisible(false);
+        lblGanancias.setVisible(false);
     }
     
 }
